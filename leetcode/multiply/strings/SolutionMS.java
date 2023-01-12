ddf53c13fe5007572525aacca0303d28cc1a6c3a
Sergey Bernatovich <SergeyBernatovich@Yandex.ru>
2023-01-12 19:04:14 +0300

    LeetCode.findBAll leetCode.longest.common.prefix lettCode.multiply.string v1.2

--- leetcode/multiply/strings/SolutionMS.java
+++ leetcode/multiply/strings/SolutionMS.java
@@ -15,46 +15,71 @@ public class SolutionMS {
             first = num1;
             second = num2;
        }
-
-                sum = sumStr ( first , second);
-
-
-
-
+            sum = sumStr ( first , second);
         return sum;
     }
     private static String sumStr (String firstN, String secondN){
         ArrayList <Integer> sumIn = new ArrayList<>();
-        for (int i = firstN.length()-1; i >=0 ; i--) {
-            sumIn.add(firstN.charAt(i)-'0');
-        }
-
+        int countMain;
         for (int i = secondN.length()-1; i >=0; i--) {
-            for (int j = 0; j <sumIn.size() ; j--) {
+            countMain =secondN.length()- 1 - i;
+            for (int j = firstN.length()-1; j >=0 ; j--) {
                 int a = secondN.charAt(i)-'0';
-                int b = sumIn.get(j);
+                int b = firstN.charAt(j)-'0';
                 int sum = a*b;
-                sumIn = resultSum (sumIn, sum, j);
+                sumIn = resultSum (sumIn, sum, countMain);
+                countMain++;
+            }
+
+        }
+        String res = "";
+        int countZero=0;
+        for (int i = sumIn.size()-1; i >=0 ; i--) {
+            res =res +sumIn.get(i);
+            if (sumIn.get(i) == 0) {
+                countZero++;
             }
         }
-
-        String res = "";
+        if (countZero == sumIn.size()) {
+            res = "0";
+        }
+   //     System.out.println(res);
         return res;
     }
-    private static ArrayList<Integer> resultSum (ArrayList<Integer> sumIn, int sum, int i){
-        sumIn.set(i,sumIn.get(i)+sum%10);
-        int count = i;
+    private static ArrayList<Integer> resultSum (ArrayList<Integer> sumIn, int sum, int countMain){
+        if (sumIn.size()-1 < countMain ) {
+            sumIn.add(sum%10);
+        } else{
+            sumIn.set(countMain, sumIn.get(countMain) + sum % 10);
+        }
+        int count = countMain;
         while (sumIn.get(count)>9){
             sumIn.set(count,sumIn.get(count)%10);
-            sumIn.set(++count,sumIn.get(count)+1);
+            if (++count == sumIn.size()){
+                sumIn.add(1);
+            } else {
+                sumIn.set(count, sumIn.get(count) + 1);
+            }
         }
+        int countSec = countMain+1;
+        if (sum/10 > 0 ) {
+            if (sumIn.size()-1 < countSec) {
+                sumIn.add(sum / 10);
+            }else {
+                sumIn.set(countSec, sumIn.get(countSec) + sum / 10);
+            }
+            while (sumIn.get(countSec) > 9) {
+                sumIn.set(countSec, sumIn.get(countSec) % 10);
+                if (++countSec == sumIn.size()) {
+                    sumIn.add(1);
+                } else {
+                    sumIn.set(countSec, sumIn.get(countSec) + 1);
 
-        int countSec = i;
-        while (sumIn.get(countSec)>9){
-            sumIn.set(countSec,sumIn.get(countSec)%10);
-            sumIn.set(++countSec,sumIn.get(countSec)+1);
+                }
+            }
         }
         
         return sumIn;
     }
+
 }
