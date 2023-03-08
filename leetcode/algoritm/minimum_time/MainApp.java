0b1a896290122ee84d1a8a96832671e066c6f491
Sergey Bernatovich <sergeybernatovich@yandex.ru>
2023-03-08 13:20:32 +0300

    LeetCode.findBAll leetCode.longest.common.prefix lettCode.multiply.string v1.2 leetCOde.TreeNOde

--- leetcode/algoritm/minimum_time/MainApp.java
+++ leetcode/algoritm/minimum_time/MainApp.java
@@ -3,10 +3,11 @@ package leetcode.algoritm.minimum_time;
 public class MainApp {
     public static void main(String[] args) {
         int [] timeTrip = {
-                1,2,3,4
+               3,4,5,6,7
+
         };
 
-        int numOfTrip = 5;
+        int numOfTrip = 20;
 
         SolutionMinimumTime solutionMinimumTime = new SolutionMinimumTime();
         System.out.println(solutionMinimumTime.minimumTime(timeTrip,numOfTrip));
--- leetcode/algoritm/minimum_time/SolutionMinimumTime.java
+++ leetcode/algoritm/minimum_time/SolutionMinimumTime.java
@@ -1,33 +1,42 @@
 package leetcode.algoritm.minimum_time;
 
 import java.util.Arrays;
+import java.util.Hashtable;
 
 public class SolutionMinimumTime {
     protected long minimumTime(int[] time, int totalTrips) {
         int ans = 0;
+        int [] count = new int[time.length];
 
          Arrays.sort(time);
+        int currTime = time[0];
+       while ( totalTrips != 0 ) {
+           int i = 0;
+           while (i < time.length && totalTrips != 0){
+           if ( currTime == time[i] * (count[i]+ 1)) {
+               count[i]++;
+               totalTrips--;
+               i++;
 
-        while (totalTrips != 0) {
-            int curTime = 1;
-
-            for (int i = 0; i < time.length; i++) {
-              if (time[i] <= curTime ){
-                  totalTrips--;
-              }
-              if (time [i] > curTime )
-
-
-
-            }
+           } else {
+               if (i < time.length - 1 && time[i+1] > currTime ) {
+                   break;
+               }
+           i++;
+           }
+           }
+           currTime++;
+       }
+        for (int i = 1; i < time.length; i++) {
 
 
-
+            ans = Math.max(time[i] * (count[i]+1), time[i-1] * (count[i-1] +1) );
         }
 
 
-        System.out.println(Arrays.toString(time));
 
-        return ans;
+        return currTime -1;
     }
 }
+// 2 5  10    20
+
