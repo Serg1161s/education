98a43a174986937657f17d1233500d48dce57fda
Sergey Bernatovich <sergeybernatovich@yandex.ru>
2023-03-15 18:18:14 +0300

    LeetCode.findBAll leetCode.longest.common.prefix lettCode.multiply.string v1.2 leetCOde.TreeNOde

--- leetcode/algoritm/bananas/MainApp.java
+++ leetcode/algoritm/bananas/MainApp.java
@@ -4,16 +4,20 @@ import java.util.Arrays;
 
 public class MainApp {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4};
+        int [] nums = {
+                1,2,3,4,5,6,7,8,9,10
+        };
 
 
 
         SolutionEating solutionEating = new SolutionEating();
 
         System.out.println(Arrays.toString(nums));
-        System.out.println(solutionEating.minEatingSpeed(nums,6));
+        System.out.println(solutionEating.minEatingSpeed(nums,15
+        ));
 
     }
 
 
 }
+// 27/ (8 - 4 +1) +1 27/5 + 1 = 6
--- leetcode/algoritm/bananas/SolutionEating.java
+++ leetcode/algoritm/bananas/SolutionEating.java
@@ -1,32 +1,38 @@
 package leetcode.algoritm.bananas;
 
 public class SolutionEating {
-    private boolean checkPiles (int [] pil, int med, int hours){
+    private boolean checkPiles (int [] pil, int med, int h){
         int i = 0;
 
-        int count = 0;
         while (i < pil.length){
-            while (pil[i] > 0){
-                pil[i] -= med;
-                count++;
-            }
+            int num = pil[i];
+            h -= (int )Math.ceil((double) num/med);
             i++;
+            if (h < 0) return false;
         }
 
-        return hours < count;
+        return true;
     }
     public int minEatingSpeed(int[] piles, int h) {
 
-        int min = piles[0];
-        int max = piles [0];
+//        int max = piles [0];
+//        int min = piles [0];
+
+       long sum = 0;
 
         for (int p : piles){
-            max = Math.max(max,p);
-            min = Math.min(min,p);
+//            max = Math.max(max,p);
+//            min = Math.min(min,p);
+            sum+=p;
+
         }
+        //int  min = (min*piles.length)/h ;
+        int n = piles.length;
+        int min =(int) (sum/h + (sum%h == 0? 0:1));
+        int max = (int)(sum / (h - n + 1)) + 1;
 
 
-        while (min <= max ){
+        while (min < max ){
             int med = min + (max - min)/2;
 
             if (checkPiles (piles,med,h)) {
